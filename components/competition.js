6ad2475a2c3d7ea5f6c693b6787a1ae0c6e5b7e7
GILGAMESH <~@gilgamesh.cc>
2020-06-08 20:57:15 -0600

    competition photo gallery

--- components/competition.js
+++ components/competition.js
@@ -1,4 +1,32 @@
 import "./banner.js";
 import { importWebElement } from "../js/fetch.js";
 
-importWebElement(`competition`);
+importWebElement(`competition`, class extends HTMLElement {
+	constructor() {
+		super();
+		setTimeout(() => {
+			// Handler for photo gallery.
+			const galleryImg = this.shadowRoot.querySelector(`[name="gallery"]>img`);
+			const photos = [
+				`./assets/competition/two-dudes.jpg`,
+				`./assets/competition/big-room-stata.jpg`
+			];
+			galleryImg.src = photos[0];
+			galleryImg.setAttribute(`idx`, `0`);
+			const intervalMs = 5000;
+			let hInterval;
+
+			const incrementPhotoGallery = () => {
+				const curIdx = Number(galleryImg.getAttribute(`idx`));
+				const nextIdx = (curIdx + 1) % photos.length;
+				galleryImg.src = photos[nextIdx];
+				galleryImg.setAttribute(`idx`, nextIdx.toString());
+				window.clearInterval(hInterval);
+				hInterval = window.setInterval(incrementPhotoGallery, intervalMs);
+			};
+
+			hInterval = window.setInterval(incrementPhotoGallery, intervalMs);
+			galleryImg.addEventListener(`click`, incrementPhotoGallery);
+		})
+	}
+});
